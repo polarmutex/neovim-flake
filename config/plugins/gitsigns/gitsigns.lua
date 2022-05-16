46f634560d15f2740d42982723753640a75491e5
Brian Ryall <brian@brianryall.xyz>
2022-05-16 13:46:05 -0400

    fix: gitsigns

--- config/plugins/gitsigns/gitsigns.lua
+++ config/plugins/gitsigns/gitsigns.lua
@@ -1,10 +1,35 @@
 require("gitsigns").setup {
     signs = {
-        add = { hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr" },
-        change = { hl = "GitSignsChange", text = "│", numhl = "GitSignsChangeNr" },
-        delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr" },
-        topdelete = { hl = "GitSignsDelete", text = "‾", numhl = "GitSignsDeleteNr" },
-        changedelete = { hl = "GitSignsDelete", text = "~", numhl = "GitSignsChangeNr" },
+        add = {
+            hl = "GitSignsAdd",
+            text = "▍",
+            numhl = "GitSignsAddNr",
+            linehl = "GitSignsAddLn"
+        },
+        change = {
+            hl = "GitSignsChange",
+            text = "▍",
+            numhl = "GitSignsChangeNr",
+            linehl = "GitSignsChangeLn",
+        },
+        delete = {
+            hl = "GitSignsDelete",
+            text = "▸",
+            numhl = "GitSignsDeleteNr",
+            linehl = "GitSignsDeleteLn",
+        },
+        topdelete = {
+            hl = "GitSignsDelete",
+            text = "▾",
+            numhl = "GitSignsDeleteNr",
+            linehl = "GitSignsDeleteLn",
+        },
+        changedelete = {
+            hl = "GitSignsChange",
+            text = "▍",
+            numhl = "GitSignsChangeNr",
+            linehl = "GitSignsChangeLn",
+        },
     },
     on_attach = function(bufnr)
         local gs = package.loaded.gitsigns
