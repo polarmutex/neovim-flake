65cf2118b956a0e120ae46f3c34ad0a0beecdb78
Brian Ryall <brian@brianryall.xyz>
2022-05-22 00:59:57 -0400

    feat: add basic functions to init

--- modules/init.nix
+++ modules/init.nix
@@ -1,9 +1,29 @@
 { lib, pkgs, ... }: {
-  vimscript = ''
-    lua << EOF
+  lua = ''
+    local ok, plenary_reload = pcall(require, "plenary.reload")
+    if not ok then
+      reloader = require
+    else
+      reloader = plenary_reload.reload_module
+    end
+
+    P = function(v)
+      print(vim.inspect(v))
+      return v
+    end
+
+    RELOAD = function(...)
+      return reloader(...)
+    end
+
+    R = function(name)
+      RELOAD(name)
+      return require(name)
+    end
+
+    vim.opt.runtimepath:append("~/repos/personal/beancount.nvim/master")
     require('polarmutex.options')
     require('polarmutex.lsp')
     require('polarmutex.treesitter')
-    EOF
   '';
 }
