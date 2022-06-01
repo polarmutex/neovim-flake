5ce7d9fd787eac44a98d52eefd1c71f121caffce
Brian Ryall <brian@brianryall.xyz>
2022-05-31 23:09:57 -0400

    fix: set current dir to lsp root

--- config/lsp/lsp.nix
+++ config/lsp/lsp.nix
@@ -2,6 +2,7 @@
 {
   lua = ''
     local function on_attach(client, bufnr)
+        vim.api.nvim_set_current_dir(client.config.root_dir)
         require('polarmutex.lsp_formatting').setup(client, bufnr)
     end
   '';
