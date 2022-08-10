8715ff942c0e8cada3d64aae7c3382f157d457d3
Brian Ryall <brian@brianryall.xyz>
2022-08-10 09:58:16 -0400

    feat: add useful autocmds

--- dotfiles/init.lua
+++ dotfiles/init.lua
@@ -1,3 +1,46 @@
 require("polarmutex.options")
 require("polarmutex.keys")
 require("polarmutex.lsp")
+
+local api = vim.api
+
+--- Remove all trailing whitespace on save
+local TrimWhiteSpaceGrp = api.nvim_create_augroup("TrimWhiteSpaceGrp", { clear = true })
+api.nvim_create_autocmd("BufWritePre", {
+    command = [[:%s/\s\+$//e]],
+    group = TrimWhiteSpaceGrp,
+})
+
+-- don't auto comment new line
+api.nvim_create_autocmd("BufEnter", { command = [[set formatoptions-=cro]] })
+
+-- Highlight on yank
+local yankGrp = api.nvim_create_augroup("YankHighlight", { clear = true })
+api.nvim_create_autocmd("TextYankPost", {
+    command = "silent! lua vim.highlight.on_yank()",
+    group = yankGrp,
+})
+
+-- go to last loc when opening a buffer
+api.nvim_create_autocmd(
+    "BufReadPost",
+    { command = [[if line("'\"") > 1 && line("'\"") <= line("$") | execute "normal! g`\"" | endif]] }
+)
+
+-- show cursor line only in active window
+local cursorGrp = api.nvim_create_augroup("CursorLine", { clear = true })
+api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
+    pattern = "*",
+    command = "set cursorline",
+    group = cursorGrp,
+})
+api.nvim_create_autocmd(
+    { "InsertEnter", "WinLeave" },
+    { pattern = "*", command = "set nocursorline", group = cursorGrp }
+)
+
+-- Enable spell checking for certain file types
+api.nvim_create_autocmd(
+    { "BufRead", "BufNewFile" },
+    { pattern = { "*.txt", "*.md", "*.tex" }, command = "setlocal spell" }
+)
