e5151cef95a2c77e9db7bd23f32522bb9605d2f4
Brian Ryall <brian@brianryall.xyz>
2022-10-20 00:10:55 -0400

    fix: doc workflow

--- dotfiles/lua/polarmutex/plugins/gitsigns.lua
+++ dotfiles/lua/polarmutex/plugins/gitsigns.lua
@@ -1,9 +1,12 @@
+---@mod polarmutex.plugins.gitsigns Gitsigns
+---@brief [[
 -- keymaps
 -- gb = { "<cmd>Gitsigns blame_line", "Blame line" },
 -- gc = { "<cmd>Gitsigns next_hunk", "Next hunk" },
 -- gn = { "<cmd>Gitsigns next_hunk", "Next hunk" },
 --  gp = { "<cmd>Gitsigns prev_hunk", "Prev hunk" },
 --  gs = { "<cmd>Gitsigns preview_hunk", "Preview hunk" },
+---@brief ]]
 
 local M = {}
 
