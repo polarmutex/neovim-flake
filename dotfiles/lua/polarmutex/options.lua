22a4f0425c08999829f4b33d002bd0c1f242cec0
Brian Ryall <brian@brianryall.xyz>
2022-10-21 16:22:33 -0400

    fix: updated neovim options

--- dotfiles/lua/polarmutex/options.lua
+++ dotfiles/lua/polarmutex/options.lua
@@ -9,6 +9,7 @@ vim.opt.listchars = "tab:→→,trail:●,nbsp:○"
 vim.opt.matchpairs = "(:),{:},[:],<:>"
 vim.opt.mouse = "a"
 vim.opt.number = true
+vim.opt.relativenumber = true
 vim.opt.scrolloff = 15
 vim.opt.showmatch = true
 vim.opt.sidescrolloff = 15
@@ -17,20 +18,26 @@ vim.opt.smartcase = true
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 vim.opt.termguicolors = true
+vim.opt.errorbells = false
+vim.opt.clipboard = "unnamedplus" -- sync with system clipboard
 --vim.opt.title = true; # Visual artifacts
 vim.opt.undofile = true
 vim.opt.updatetime = 100
-vim.opt.visualbell = true
 vim.opt.wildignore = "*/tmp/*,*.so,*.swp,*.pyc,*.db,*.sqlite,*.class,*/node_modules/*,*/.git/*"
 vim.opt.wildmode = "list:longest,list:full"
 vim.opt.wrap = false
 
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
-vim.opt.smartindent = true
 vim.opt.softtabstop = 4
 vim.opt.tabstop = 4
+
+vim.opt.splitbelow = true -- Put new windows below current
+vim.opt.splitright = true -- Put new windows right of current
 
 vim.opt.cursorline = true
 
 vim.opt.laststatus = 3
+
+vim.opt.cmdheight = 0
+vim.opt.splitkeep = "screen"
