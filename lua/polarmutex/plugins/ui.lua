ed149f2b3ebe190cfed992e3b29a5833568d7f47
Brian Ryall <brian@brianryall.xyz>
2023-01-23 18:36:32 -0500

    feat(ui): add new statuscolumn

--- lua/polarmutex/plugins/ui.lua
+++ lua/polarmutex/plugins/ui.lua
@@ -497,12 +497,70 @@ statusline_spec.config = function()
         }),
     }
 
+    local Stc = {
+        {
+            provider = "%s",
+            static = {
+                resolve = function(self, name)
+                    for pat, cb in pairs(self.handlers) do
+                        if name:match(pat) then
+                            return cb
+                        end
+                    end
+                end,
+
+                handlers = {
+                    ["GitSigns.*"] = function(args)
+                        require("gitsigns").preview_hunk_inline()
+                    end,
+                    ["Dap.*"] = function(args)
+                        require("dap").toggle_breakpoint()
+                    end,
+                    ["Diagnostic.*"] = function(args)
+                        vim.diagnostic.open_float() -- { pos = args.mousepos.line - 1, relative = "mouse" })
+                    end,
+                },
+            },
+            on_click = {
+                callback = function(self, ...)
+                    local mousepos = vim.fn.getmousepos()
+                    vim.api.nvim_win_set_cursor(0, { mousepos.line, mousepos.column })
+                    local sign_at_cursor = vim.fn.screenstring(mousepos.screenrow, mousepos.screencol)
+                    if sign_at_cursor ~= "" then
+                        local args = {
+                            mousepos = mousepos,
+                        }
+                        local signs = vim.fn.sign_getdefined()
+                        for _, sign in ipairs(signs) do
+                            local glyph = sign.text:gsub(" ", "")
+                            if sign_at_cursor == glyph then
+                                vim.defer_fn(function()
+                                    self:resolve(sign.name)(args)
+                                end, 10)
+                                return
+                            end
+                        end
+                    end
+                end,
+                name = "heirline_signcol_callback",
+                update = true,
+            },
+        },
+        {
+            provider = "%=%4{v:wrap ? '' : &nu ? (&rnu && v:relnum ? v:relnum : v:lnum) : ''}",
+        },
+        {
+            provider = "%C ",
+        },
+    }
+
     require("heirline").setup({
         statusline = StatusLines,
         winbar = WinBar,
+        statuscolumn = Stc,
         --tabline
-        --statuscolumn
     })
+    vim.o.statuscolumn = "%=%{v:virtnum < 1 ? (v:relnum ? v:relnum : v:lnum < 10 ? v:lnum . '  ' : v:lnum) : ''}%=%s" --require("heirline").eval_statuscolumn()
 
     vim.api.nvim_create_augroup("Heirline", { clear = true })
     vim.api.nvim_create_autocmd("User", {
