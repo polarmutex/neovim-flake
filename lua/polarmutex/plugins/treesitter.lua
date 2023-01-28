5b28f4387f78ab6cd34df066f505f31de88aac82
Brian Ryall <brian@brianryall.xyz>
2023-01-27 23:00:15 -0500

    fix(treesitter): enable playground

--- lua/polarmutex/plugins/treesitter.lua
+++ lua/polarmutex/plugins/treesitter.lua
@@ -18,6 +18,24 @@ treesitter_spec.config = function()
             use_languagetree = true,
             additional_vim_regex_highlighting = true,
         },
+        playground = {
+            enable = true,
+            disable = {},
+            updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+            persist_queries = false, -- Whether the query persists across vim sessions
+            keybindings = {
+                --toggle_query_editor = "o",
+                --toggle_hl_groups = "i",
+                --toggle_injected_languages = "t",
+                --toggle_anonymous_nodes = "a",
+                --toggle_language_display = "I",
+                --focus_language = "f",
+                --unfocus_language = "F",
+                --update = "R",
+                --goto_node = "<cr>",
+                --show_help = "?",
+            },
+        },
     })
     local ft_to_parser = require("nvim-treesitter.parsers").filetype_to_parsername
     ft_to_parser.xml = "html"
