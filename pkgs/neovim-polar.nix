0f69fd9d674d7ac067c2a248c30de479828f2d18
Brian Ryall <brian@brianryall.xyz>
2023-03-24 15:58:58 -0400

    fix: unused and add aliases

--- pkgs/neovim-polar.nix
+++ pkgs/neovim-polar.nix
@@ -1,35 +1,33 @@
-{ pkgs
-, lib
-, neovim
-, ...
-}:
-let
+{
+  pkgs,
+  lib,
+  neovim,
+  ...
+}: let
   neovimConfig = pkgs.neovimUtils.makeNeovimConfig {
+    viAlias = true;
+    vimAlias = true;
+
+    withPython3 = true;
+    withRuby = true;
+    withNodeJs = true;
+
     customRC = ''
       lua << EOF
       -- bootstrap lazy.nvim, LazyVim and your plugins
       require('polarmutex.config.lazy').setup()
       EOF
     '';
-    plugins =
-      let
-        withSrc = pkg: src: pkg.overrideAttrs (_: { inherit src; });
-        plugin = pname: src:
-          pkgs.vimUtils.buildVimPluginFrom2Nix {
-            inherit pname src;
-            version = "master";
-          };
-      in
-      [
-        {
-          plugin = pkgs.neovim-lua-config-polar;
-          optional = false;
-        }
-      ];
+    plugins = [
+      {
+        plugin = pkgs.neovim-lua-config-polar;
+        optional = false;
+      }
+    ];
   };
 in
-pkgs.wrapNeovimUnstable neovim.packages.${pkgs.system}.default
+  pkgs.wrapNeovimUnstable neovim.packages.${pkgs.system}.default
   (neovimConfig
     // {
-    wrapRc = true;
-  })
+      wrapRc = true;
+    })
