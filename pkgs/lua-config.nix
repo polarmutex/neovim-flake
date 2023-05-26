adc5de00719e40b54cef8ba65ed4444f5d411e6c
Brian Ryall <brian@brianryall.xyz>
2023-05-26 00:16:17 -0400

    refactor: substitute packages

--- pkgs/lua-config.nix
+++ pkgs/lua-config.nix
@@ -11,12 +11,6 @@
     excludeFiles ? [],
   }: let
     pname = "${moduleName}";
-    luaSrc =
-      builtins.filterSource
-      (path: type:
-        (lib.hasSuffix ".lua" path)
-        && ! (lib.lists.any (x: baseNameOf path == x) excludeFiles))
-      configDir;
   in (pkgs.vimUtils.buildVimPluginFrom2Nix {
     inherit pname;
     version = "dev";
@@ -31,9 +25,10 @@
           > $sourceRoot/doc/polarmutex.txt
     '';
     postInstall = let
+      inherit (builtins) attrNames attrValues;
       subs =
         lib.concatStringsSep " "
-        (lib.lists.zipListsWith (f: t: "--subst-var-by ${f} ${t}") vars replacements);
+        (lib.lists.zipListsWith (f: t: "--subst-var-by ${f} ${t}") (attrNames vars) (attrValues vars));
     in
       ''''
       + lib.optionalString
@@ -51,141 +46,128 @@
       maintainers = [maintainers.polarmutex];
     };
   });
-
-  buildPlugin = pname: src:
-  # TODO build neovim plugin
-    pkgs.vimUtils.buildVimPluginFrom2Nix {
-      inherit pname src;
-      version = src.revision;
-    };
 in
   buildLuaConfigPlugin {
     configDir = ../lua;
     moduleName = "polarmutex";
     excludeFiles = []; #if builtins.isNull config then [ ] else [ "user.lua" ];
-    vars = [
-      "astro.language-server"
-      "beancount.beancount-language-server"
-      "cpp.clangd"
-      "go.gopls"
-      "json.jsonls"
-      "java.debug.plugin"
-      "java.jdt-language-server"
-      "rust.dprint"
-      "lua.sumneko-lua-language-server"
-      "lua.stylua"
-      "nix.nil"
-      "nix.alejandra"
-      "python.pyright"
-      "rust.analyzer"
-      "rust.clippy"
-      "svelte.svelte-language-server"
-      "typescript.typescript-language-server"
+    vars = {
+      "astro.language-server" = lib.getExe pkgs.nodePackages."@astrojs/language-server";
+      "beancount.beancount-language-server" = pkgs.beancount-language-server;
+      "cpp.clangd" = pkgs.clang-tools;
+      "go.gopls" =
+        pkgs.gopls;
+      "json.jsonls" =
+        lib.getExe pkgs.nodePackages.vscode-json-languageserver;
+      "java.debug.plugin" =
+        (pkgs.fetchMavenArtifact
+          {
+            groupId = "com.microsoft.java";
+            artifactId = "com.microsoft.java.debug.plugin";
+            version = "0.34.0";
+            sha256 = "sha256-vKvTHA17KPhvxCwI6XdQX3Re2z7vyMhObM9l3QOcrAM=";
+          })
+        .jar;
+      "java.jdt-language-server" =
+        pkgs.jdt-language-server;
+      "rust.dprint" =
+        pkgs.dprint;
+      "lua.sumneko-lua-language-server" =
+        pkgs.lua-language-server;
+      "lua.stylua" =
+        pkgs.stylua;
+      "nix.nil" =
+        lib.getExe pkgs.nil;
+      "nix.alejandra" =
+        pkgs.alejandra;
+      "python.pyright" =
+        pkgs.pyright;
+      "rust.analyzer" =
+        pkgs.lib.getExe pkgs.rust-analyzer;
+      "rust.clippy" =
+        pkgs.clippy;
+      "svelte.svelte-language-server" =
+        pkgs.lib.getExe pkgs.nodePackages.svelte-language-server;
+      "typescript.typescript-language-server" =
+        lib.getExe pkgs.nodePackages.typescript-language-server;
 
-      "neovimPlugin.beancount-nvim"
-      "neovimPlugin.cmp-nvim-lsp"
-      "neovimPlugin.cmp-path"
-      "neovimPlugin.cmp-omni"
-      "neovimPlugin.cmp-calc"
-      "neovimPlugin.cmp-buffer"
-      "neovimPlugin.cmp-cmdline"
-      "neovimPlugin.cmp-dap"
-      "neovimPlugin.crates-nvim"
-      "neovimPlugin.diffview-nvim"
-      "neovimPlugin.gitsigns-nvim"
-      "neovimPlugin.gitworktree-nvim"
-      "neovimPlugin.harpoon"
-      "neovimPlugin.heirline-nvim"
-      "neovimPlugin.lazy-nvim"
-      "neovimPlugin.lspkind-nvim"
-      "neovimPlugin.lspformat-nvim"
-      "neovimPlugin.neodev-nvim"
-      "neovimPlugin.neogit"
-      "neovimPlugin.noice-nvim"
-      "neovimPlugin.nui-nvim"
-      "neovimPlugin.null-ls-nvim"
-      "neovimPlugin.nvim-cmp"
-      "neovimPlugin.nvim-colorizer"
-      "neovimPlugin.nvim-dap"
-      "neovimPlugin.nvim-dap-python"
-      "neovimPlugin.nvim-dap-ui"
-      "neovimPlugin.nvim-dap-virtual-text"
-      "neovimPlugin.nvim-lspconfig"
-      "neovimPlugin.nvim-jdtls"
-      "neovimPlugin.nvim-treesitter"
-      "neovimPlugin.nvim-treesitter-playground"
-      "neovimPlugin.one-small-step-for-vimkind"
-      "neovimPlugin.plenary-nvim"
-      "neovimPlugin.rust-tools-nvim"
-      "neovimPlugin.telescope-nvim"
-      "neovimPlugin.tokyonight-nvim"
-      "neovimPlugin.vim-be-good"
-      "neovimPlugin.nvim-web-devicons"
-    ];
-    replacements = [
-      (lib.getExe pkgs.nodePackages."@astrojs/language-server")
-      (pkgs.beancount-language-server)
-      (pkgs.clang-tools)
-      (pkgs.gopls)
-      (lib.getExe pkgs.nodePackages.vscode-json-languageserver)
-      (pkgs.fetchMavenArtifact
-        {
-          groupId = "com.microsoft.java";
-          artifactId = "com.microsoft.java.debug.plugin";
-          version = "0.34.0";
-          sha256 = "sha256-vKvTHA17KPhvxCwI6XdQX3Re2z7vyMhObM9l3QOcrAM=";
-        })
-      .jar
-      (pkgs.jdt-language-server)
-      (pkgs.dprint)
-      (pkgs.lua-language-server)
-      (pkgs.stylua)
-      (lib.getExe pkgs.nil)
-      (pkgs.alejandra)
-      (pkgs.pyright)
-      (pkgs.lib.getExe pkgs.rust-analyzer)
-      (pkgs.clippy)
-      (pkgs.lib.getExe pkgs.nodePackages.svelte-language-server)
-      (lib.getExe pkgs.nodePackages.typescript-language-server)
-
-      pkgs.neovimPlugins.beancount-nvim
-      pkgs.neovimPlugins.cmp-nvim-lsp
-      pkgs.neovimPlugins.cmp-path
-      pkgs.neovimPlugins.cmp-omni
-      pkgs.neovimPlugins.cmp-calc
-      pkgs.neovimPlugins.cmp-buffer
-      pkgs.neovimPlugins.cmp-cmdline
-      pkgs.neovimPlugins.cmp-dap
-      pkgs.neovimPlugins.crates-nvim
-      pkgs.neovimPlugins.diffview-nvim
-      pkgs.neovimPlugins.gitsigns-nvim
-      pkgs.neovimPlugins.git-worktree-nvim
-      pkgs.neovimPlugins.harpoon
-      pkgs.neovimPlugins.heirline-nvim
-      pkgs.neovimPlugins.lazy-nvim
-      pkgs.neovimPlugins.lspkind-nvim
-      pkgs.neovimPlugins.lsp-format-nvim
-      pkgs.neovimPlugins.neodev-nvim
-      pkgs.neovimPlugins.neogit
-      pkgs.neovimPlugins.noice-nvim
-      pkgs.neovimPlugins.nui-nvim
-      pkgs.neovimPlugins.null-ls-nvim
-      pkgs.neovimPlugins.nvim-cmp
-      pkgs.neovimPlugins.nvim-colorizer
-      pkgs.neovimPlugins.nvim-dap
-      pkgs.neovimPlugins.nvim-dap-python
-      pkgs.neovimPlugins.nvim-dap-ui
-      pkgs.neovimPlugins.nvim-dap-virtual-text
-      pkgs.neovimPlugins.nvim-lspconfig
-      (pkgs.neovimPlugins.nvim-jdtls)
-      pkgs.nvim-treesitter-master
-      pkgs.neovimPlugins.nvim-treesitter-playground
-      pkgs.neovimPlugins.one-small-step-for-vimkind
-      pkgs.neovimPlugins.plenary-nvim
-      pkgs.neovimPlugins.rust-tools-nvim
-      pkgs.neovimPlugins.telescope-nvim
-      pkgs.neovimPlugins.tokyonight-nvim
-      pkgs.neovimPlugins.vim-be-good
-      pkgs.neovimPlugins.nvim-web-devicons
-    ];
+      "neovimPlugin.beancount-nvim" =
+        pkgs.neovimPlugins.beancount-nvim;
+      "neovimPlugin.cmp-nvim-lsp" =
+        pkgs.neovimPlugins.cmp-nvim-lsp;
+      "neovimPlugin.cmp-path" =
+        pkgs.neovimPlugins.cmp-path;
+      "neovimPlugin.cmp-omni" =
+        pkgs.neovimPlugins.cmp-omni;
+      "neovimPlugin.cmp-calc" =
+        pkgs.neovimPlugins.cmp-calc;
+      "neovimPlugin.cmp-buffer" =
+        pkgs.neovimPlugins.cmp-buffer;
+      "neovimPlugin.cmp-cmdline" =
+        pkgs.neovimPlugins.cmp-cmdline;
+      "neovimPlugin.cmp-dap" =
+        pkgs.neovimPlugins.cmp-dap;
+      "neovimPlugin.crates-nvim" =
+        pkgs.neovimPlugins.crates-nvim;
+      "neovimPlugin.diffview-nvim" =
+        pkgs.neovimPlugins.diffview-nvim;
+      "neovimPlugin.gitsigns-nvim" =
+        pkgs.neovimPlugins.gitsigns-nvim;
+      "neovimPlugin.gitworktree-nvim" =
+        pkgs.neovimPlugins.git-worktree-nvim;
+      "neovimPlugin.harpoon" =
+        pkgs.neovimPlugins.harpoon;
+      "neovimPlugin.heirline-nvim" =
+        pkgs.neovimPlugins.heirline-nvim;
+      "neovimPlugin.lazy-nvim" =
+        pkgs.neovimPlugins.lazy-nvim;
+      "neovimPlugin.lspkind-nvim" =
+        pkgs.neovimPlugins.lspkind-nvim;
+      "neovimPlugin.lspformat-nvim" =
+        pkgs.neovimPlugins.lsp-format-nvim;
+      "neovimPlugin.neodev-nvim" =
+        pkgs.neovimPlugins.neodev-nvim;
+      "neovimPlugin.neogit" =
+        pkgs.neovimPlugins.neogit;
+      "neovimPlugin.noice-nvim" =
+        pkgs.neovimPlugins.noice-nvim;
+      "neovimPlugin.nui-nvim" =
+        pkgs.neovimPlugins.nui-nvim;
+      "neovimPlugin.null-ls-nvim" =
+        pkgs.neovimPlugins.null-ls-nvim;
+      "neovimPlugin.nvim-cmp" =
+        pkgs.neovimPlugins.nvim-cmp;
+      "neovimPlugin.nvim-colorizer" =
+        pkgs.neovimPlugins.nvim-colorizer;
+      "neovimPlugin.nvim-dap" =
+        pkgs.neovimPlugins.nvim-dap;
+      "neovimPlugin.nvim-dap-python" =
+        pkgs.neovimPlugins.nvim-dap-python;
+      "neovimPlugin.nvim-dap-ui" =
+        pkgs.neovimPlugins.nvim-dap-ui;
+      "neovimPlugin.nvim-dap-virtual-text" =
+        pkgs.neovimPlugins.nvim-dap-virtual-text;
+      "neovimPlugin.nvim-lspconfig" =
+        pkgs.neovimPlugins.nvim-lspconfig;
+      "neovimPlugin.nvim-jdtls" =
+        pkgs.neovimPlugins.nvim-jdtls;
+      "neovimPlugin.nvim-treesitter" =
+        pkgs.nvim-treesitter-master;
+      "neovimPlugin.nvim-treesitter-playground" =
+        pkgs.neovimPlugins.nvim-treesitter-playground;
+      "neovimPlugin.one-small-step-for-vimkind" =
+        pkgs.neovimPlugins.one-small-step-for-vimkind;
+      "neovimPlugin.plenary-nvim" =
+        pkgs.neovimPlugins.plenary-nvim;
+      "neovimPlugin.rust-tools-nvim" =
+        pkgs.neovimPlugins.rust-tools-nvim;
+      "neovimPlugin.telescope-nvim" =
+        pkgs.neovimPlugins.telescope-nvim;
+      "neovimPlugin.tokyonight-nvim" =
+        pkgs.neovimPlugins.tokyonight-nvim;
+      "neovimPlugin.vim-be-good" =
+        pkgs.neovimPlugins.vim-be-good;
+      "neovimPlugin.nvim-web-devicons" =
+        pkgs.neovimPlugins.nvim-web-devicons;
+    };
   }
