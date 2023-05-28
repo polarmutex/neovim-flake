3965c0872fb84e03bd4985abcac019d39fc5b11f
Brian Ryall <brian@brianryall.xyz>
2023-05-27 23:14:08 -0400

    fix(neogit): blank confirm box on save

--- config/lua/polarmutex/plugins/git.lua
+++ config/lua/polarmutex/plugins/git.lua
@@ -50,6 +50,7 @@ neogit_spec.config = function()
     require("neogit").setup({
         kind = "split",
         disable_builtin_notifications = true,
+        disable_commit_confirmation = true,
         signs = {
             section = { "", "" },
             item = { "", "" },
