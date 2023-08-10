bc5fd088019b342545b6fa221e3690949d881d99
Brian Ryall <brian@brianryall.xyz>
2023-08-09 23:30:11 -0400

    feat: add rust

--- pkgs/lua-config.nix
+++ pkgs/lua-config.nix
@@ -205,7 +205,17 @@
             ruff_lsp = {
               cmd = [(lib.getExe pkgs.ruff-lsp)];
             };
-            #rust = rust-analyzer;
+            rust_analyzer = {
+              cmd = [(lib.getExe pkgs.rust-analyzer)];
+              settings = rawLua ''
+                {
+                  ["rust-analyzer"] = {
+                    checkOnSave = {
+                      command = "clippy";
+                    };
+                  }
+                }'';
+            };
           };
           # you can do any additional lsp server setup here
           # return true if you don't want this server to be setup with lspconfig
