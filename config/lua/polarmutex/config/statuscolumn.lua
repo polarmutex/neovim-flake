952965443418f23890815167efc8919445a3b903
Brian Ryall <brian@brianryall.xyz>
2023-10-16 23:04:27 -0400

    fix: gitsigns not showing

--- config/lua/polarmutex/config/statuscolumn.lua
+++ config/lua/polarmutex/config/statuscolumn.lua
@@ -1,11 +1,64 @@
 local M = {}
 
+---@alias Sign {name:string, text:string, texthl:string, priority:number}
+
 ---@return {name:string, text:string, texthl:string}[]
-function M.get_signs(win)
-    local buf = vim.api.nvim_win_get_buf(win)
-    return vim.tbl_map(function(sign)
-        return vim.fn.sign_getdefined(sign.name)[1]
-    end, vim.fn.sign_getplaced(buf, { group = "*", lnum = vim.v.lnum })[1].signs)
+function M.get_signs(buf, lnum)
+    -- Get regular signs
+    ---@type Sign[]
+    local signs = vim.tbl_map(function(sign)
+        ---@type Sign
+        local ret = vim.fn.sign_getdefined(sign.name)[1]
+        ret.priority = sign.priority
+        return ret
+    end, vim.fn.sign_getplaced(buf, { group = "*", lnum = lnum })[1].signs)
+
+    -- Get extmark signs
+    local extmarks = vim.api.nvim_buf_get_extmarks(
+        buf,
+        -1,
+        { lnum - 1, 0 },
+        { lnum - 1, -1 },
+        { details = true, type = "sign" }
+    )
+    for _, extmark in pairs(extmarks) do
+        signs[#signs + 1] = {
+            name = extmark[4].sign_hl_group or "",
+            text = extmark[4].sign_text,
+            texthl = extmark[4].sign_hl_group,
+            priority = extmark[4].priority,
+        }
+    end
+
+    -- Sort by priority
+    table.sort(signs, function(a, b)
+        return (a.priority or 0) < (b.priority or 0)
+    end)
+
+    return signs
+end
+
+---@return Sign?
+---@param buf number
+---@param lnum number
+function M.get_mark(buf, lnum)
+    local marks = vim.fn.getmarklist(buf)
+    vim.list_extend(marks, vim.fn.getmarklist())
+    for _, mark in ipairs(marks) do
+        if mark.pos[1] == buf and mark.pos[2] == lnum and mark.mark:match("[a-zA-Z]") then
+            return { text = mark.mark:sub(2), texthl = "DiagnosticHint" }
+        end
+    end
+end
+
+---@param sign? Sign
+---@param len? number
+function M.icon(sign, len)
+    sign = sign or {}
+    len = len or 2
+    local text = vim.fn.strcharpart(sign.text or "", 0, len) ---@type string
+    text = text .. string.rep(" ", len - vim.fn.strchars(text))
+    return sign.texthl and ("%#" .. sign.texthl .. "#" .. text .. "%*") or text
 end
 
 function M.column()
@@ -13,26 +66,51 @@ function M.column()
     if vim.wo[win].signcolumn == "no" then
         return ""
     end
-    local sign, git_sign
-    for _, s in ipairs(M.get_signs(win)) do
-        if s.name:find("GitSign") then
-            git_sign = s
+    local buf = vim.api.nvim_win_get_buf(win)
+
+    ---@type Sign?,Sign?,Sign?
+    local left, right, fold
+    for _, s in ipairs(M.get_signs(buf, vim.v.lnum)) do
+        if s.name and s.name:find("GitSign") then
+            right = s
         else
-            sign = s
+            left = s
         end
     end
 
-    local nu = " "
-    if vim.wo[win].number and vim.wo[win].relativenumber and vim.v.virtnum == 0 then
-        nu = vim.v.relnum == 0 and vim.v.lnum or vim.v.relnum
+    if vim.v.virtnum ~= 0 then
+        left = nil
     end
-    local components = {
-        sign and ("%#" .. (sign.texthl or "DiagnosticInfo") .. "#" .. sign.text .. "%*") or "  ",
+
+    vim.api.nvim_win_call(win, function()
+        if vim.fn.foldclosed(vim.v.lnum) >= 0 then
+            fold = { text = vim.opt.fillchars:get().foldclose or "", texthl = "Folded" }
+        end
+    end)
+
+    local nu = ""
+    if vim.wo[win].number and vim.v.virtnum == 0 then
+        nu = vim.wo[win].relativenumber and vim.v.relnum ~= 0 and vim.v.relnum or vim.v.lnum
+    end
+
+    return table.concat({
+        M.icon(M.get_mark(buf, vim.v.lnum) or left),
         [[%=]],
         nu .. " ",
-        git_sign and ("%#" .. git_sign.texthl .. "#" .. git_sign.text .. "%*") or "  ",
-    }
-    return table.concat(components, "")
+        M.icon(fold or right),
+    }, "")
+
+    --local nu = " "
+    --if vim.wo[win].number and vim.wo[win].relativenumber and vim.v.virtnum == 0 then
+    --    nu = vim.v.relnum == 0 and vim.v.lnum or vim.v.relnum
+    --end
+    --local components = {
+    --    sign and ("%#" .. (sign.texthl or "DiagnosticInfo") .. "#" .. sign.text .. "%*") or "  ",
+    --    [[%=]],
+    --    nu .. " ",
+    --    git_sign and ("%#" .. git_sign.texthl .. "#" .. git_sign.text .. "%*") or "  ",
+    -- }
+    -- return table.concat(components, "")
 end
 
 M.setup = function()
