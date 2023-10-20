a48293d4a92a4ba51bf57d4d87dd8c714fcb8629
GitHub <noreply@github.com>
2023-10-20 04:20:09 +0000

    chore: Bump pkgs/plugins/nvim-lspconfig/nvfetcher.toml
    Co-authored-by: polarmutex <polarmutex@users.noreply.github.com>

--- pkgs/plugins/nvim-lspconfig/generated.nix
+++ pkgs/plugins/nvim-lspconfig/generated.nix
@@ -3,14 +3,14 @@
 {
   nvim-lspconfig = {
     pname = "nvim-lspconfig";
-    version = "bb8e3dfbe10d80c294bb609c4bb4bba3ecdfbfe2";
+    version = "e49b1e90c1781ce372013de3fa93a91ea29fc34a";
     src = fetchFromGitHub {
       owner = "neovim";
       repo = "nvim-lspconfig";
-      rev = "bb8e3dfbe10d80c294bb609c4bb4bba3ecdfbfe2";
+      rev = "e49b1e90c1781ce372013de3fa93a91ea29fc34a";
       fetchSubmodules = false;
-      sha256 = "sha256-T4ssH7zzzfLdh9ilLfzp8ZqACK504ENgH2i31CSE5hc=";
+      sha256 = "sha256-iNVg2hiGF+DNz+XPDlPjw+Sf3crfsK2M3dfs1wYbY10=";
     };
-    date = "2023-10-13";
+    date = "2023-10-16";
   };
 }
