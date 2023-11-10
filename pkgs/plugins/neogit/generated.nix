2abf0955f635dea7ad05372237e3a9de61531085
GitHub <noreply@github.com>
2023-11-10 04:19:14 +0000

    chore: Bump pkgs/plugins/neogit/nvfetcher.toml
    Co-authored-by: polarmutex <polarmutex@users.noreply.github.com>

--- pkgs/plugins/neogit/generated.nix
+++ pkgs/plugins/neogit/generated.nix
@@ -3,14 +3,14 @@
 {
   neogit = {
     pname = "neogit";
-    version = "5a2d4688a80d67dc224375c78e62ffd7dc1e79e6";
+    version = "d3c5687a78cffc25026ff31d2fcbd61a2a3e067f";
     src = fetchFromGitHub {
       owner = "NeogitOrg";
       repo = "neogit";
-      rev = "5a2d4688a80d67dc224375c78e62ffd7dc1e79e6";
+      rev = "d3c5687a78cffc25026ff31d2fcbd61a2a3e067f";
       fetchSubmodules = false;
-      sha256 = "sha256-SxgXD0b0obNRpOX/R8+zIUS2kG//ryLMdGTrf1apwFU=";
+      sha256 = "sha256-04sFlXn6wqa1XIzUygp17vN1Kv9qKWsFDGqf/mpn2Qs=";
     };
-    date = "2023-10-31";
+    date = "2023-11-08";
   };
 }
