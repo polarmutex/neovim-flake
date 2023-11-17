03f6849d966a9f0d3d383627694e06d927d02edf
GitHub <noreply@github.com>
2023-11-17 04:20:31 +0000

    chore: Bump pkgs/plugins/nvim-lint/nvfetcher.toml
    Co-authored-by: polarmutex <polarmutex@users.noreply.github.com>

--- pkgs/plugins/nvim-lint/generated.nix
+++ pkgs/plugins/nvim-lint/generated.nix
@@ -3,14 +3,14 @@
 {
   nvim-lint = {
     pname = "nvim-lint";
-    version = "bd9bcc0937614cd94c5bde56f110ddc15a76c08c";
+    version = "9e096df0858a1834ec2a60abe6cceb4d84dbd34c";
     src = fetchFromGitHub {
       owner = "mfussenegger";
       repo = "nvim-lint";
-      rev = "bd9bcc0937614cd94c5bde56f110ddc15a76c08c";
+      rev = "9e096df0858a1834ec2a60abe6cceb4d84dbd34c";
       fetchSubmodules = false;
-      sha256 = "sha256-4PgHooJ2YUgKEYWtz+ss7Ok9qzPUi/YcwEu2YrrhkOk=";
+      sha256 = "sha256-gpyk0XgR1nzGz1CLDvHYm81w/0CuxUCKoa6QfRbDtMI=";
     };
-    date = "2023-11-09";
+    date = "2023-11-16";
   };
 }
