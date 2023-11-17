4616f0aa9c9183dd32a36b03fb5d4b4a8456980f
GitHub <noreply@github.com>
2023-11-17 04:20:30 +0000

    chore: Bump pkgs/plugins/neodev-nvim/nvfetcher.toml
    Co-authored-by: polarmutex <polarmutex@users.noreply.github.com>

--- pkgs/plugins/neodev-nvim/generated.nix
+++ pkgs/plugins/neodev-nvim/generated.nix
@@ -3,14 +3,14 @@
 {
   neodev-nvim = {
     pname = "neodev-nvim";
-    version = "b90ec1b35dc67902bd3c4fbd80ab11993c1af8cc";
+    version = "b2881eeb395d2b268de5fe9b5e201a8f1816beb8";
     src = fetchFromGitHub {
       owner = "folke";
       repo = "neodev.nvim";
-      rev = "b90ec1b35dc67902bd3c4fbd80ab11993c1af8cc";
+      rev = "b2881eeb395d2b268de5fe9b5e201a8f1816beb8";
       fetchSubmodules = false;
-      sha256 = "sha256-RRPjwPMRVupZVndl9LiWbyE8YMk6ydwTuhcMLJKL8yM=";
+      sha256 = "sha256-CRPbXQvxX4dfrbs1rDA64XwGKGKBlYJ8M19fttmalgs=";
     };
-    date = "2023-11-09";
+    date = "2023-11-15";
   };
 }
