523e1d1d8873f19ab73842c84e1fd0a43c9e7e30
GitHub <noreply@github.com>
2023-12-08 04:19:34 +0000

    chore: Bump pkgs/plugins/conform-nvim/nvfetcher.toml
    Co-authored-by: polarmutex <polarmutex@users.noreply.github.com>

--- pkgs/plugins/conform-nvim/generated.nix
+++ pkgs/plugins/conform-nvim/generated.nix
@@ -3,13 +3,13 @@
 {
   conform-nvim = {
     pname = "conform-nvim";
-    version = "v4.2.0";
+    version = "v4.3.0";
     src = fetchFromGitHub {
       owner = "stevearc";
       repo = "conform.nvim";
-      rev = "v4.2.0";
+      rev = "v4.3.0";
       fetchSubmodules = false;
-      sha256 = "sha256-odqRoDqFxYUAQZ8hsEG6y1MBqF7rbQCdKyyGlCim4Rw=";
+      sha256 = "sha256-vw9uUhnsSj2Iv3NxcoyYpemrj3cGjQT7ezyn7d187Do=";
     };
   };
 }
