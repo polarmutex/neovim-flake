7e13324bb762231c98a51dd194082f38955e5af9
GitHub <noreply@github.com>
2023-12-15 04:20:24 +0000

    chore: Bump pkgs/plugins/nvim-cmp/nvfetcher.toml
    Co-authored-by: polarmutex <polarmutex@users.noreply.github.com>

--- pkgs/plugins/nvim-cmp/generated.nix
+++ pkgs/plugins/nvim-cmp/generated.nix
@@ -3,14 +3,14 @@
 {
   nvim-cmp = {
     pname = "nvim-cmp";
-    version = "0b751f6beef40fd47375eaf53d3057e0bfa317e4";
+    version = "538e37ba87284942c1d76ed38dd497e54e65b891";
     src = fetchFromGitHub {
       owner = "hrsh7th";
       repo = "nvim-cmp";
-      rev = "0b751f6beef40fd47375eaf53d3057e0bfa317e4";
+      rev = "538e37ba87284942c1d76ed38dd497e54e65b891";
       fetchSubmodules = false;
-      sha256 = "sha256-auUHEjUDOaDkMI9loN6T+/whDDNzPl04bf+nqqLQ5+I=";
+      sha256 = "sha256-BtAYRYn6m788zAq/mNnbAzAxp1TGf9QkRE0hSOp9sdc=";
     };
-    date = "2023-11-06";
+    date = "2023-12-14";
   };
 }
