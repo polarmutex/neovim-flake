9e2aa924b70ec8abd8f12915dca5a8c5ce0718a5
GitHub <noreply@github.com>
2023-12-15 04:19:41 +0000

    chore: Bump pkgs/plugins/gitsigns-nvim/nvfetcher.toml
    Co-authored-by: polarmutex <polarmutex@users.noreply.github.com>

--- pkgs/plugins/gitsigns-nvim/generated.nix
+++ pkgs/plugins/gitsigns-nvim/generated.nix
@@ -3,14 +3,14 @@
 {
   gitsigns-nvim = {
     pname = "gitsigns-nvim";
-    version = "e93a158b8773946dc9940a4321d35c1b52c8e293";
+    version = "d195f0c35ced5174d3ecce1c4c8ebb3b5bc23fa9";
     src = fetchFromGitHub {
       owner = "lewis6991";
       repo = "gitsigns.nvim";
-      rev = "e93a158b8773946dc9940a4321d35c1b52c8e293";
+      rev = "d195f0c35ced5174d3ecce1c4c8ebb3b5bc23fa9";
       fetchSubmodules = false;
-      sha256 = "sha256-U0PbdVOvChyc3H3azzQcLGjfPX+gzs+C952lpEKvWOU=";
+      sha256 = "sha256-lzL+1OOS0bpMeB9SCkc147/OdweI206r0yPFBmhsN1g=";
     };
-    date = "2023-12-07";
+    date = "2023-12-12";
   };
 }
