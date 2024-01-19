132003a4f816bd6707025e9a5acfa1898acd014e
Brian Ryall <me@brianryall.xyz>
2024-01-19 10:18:54 -0500

    fix: add markdown to conform

--- nvim/plugin/conform.lua
+++ nvim/plugin/conform.lua
@@ -1,6 +1,7 @@
 require("conform").setup({
     formatters_by_ft = {
         lua = { "stylua" },
+        markdown = { "mdformat" },
         -- Conform will run multiple formatters sequentially
         --python = { "isort", "black" },
         -- Use a sub-list to run only the first available formatter
