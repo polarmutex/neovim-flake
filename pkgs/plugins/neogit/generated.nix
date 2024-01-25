d016998a7023bfef3eb8036a4d8a73b5d249f6e3
polarmutex <polarmutex@users.noreply.github.com>
2024-01-25 21:13:47 +0000

    chore(plugin-update):  neogit: 38dd297a905ec6869f4c20ea9184a3e514316e3b → ce0c369ccdba3f644a3b28f4c053421f435352c9

--- pkgs/plugins/neogit/generated.nix
+++ pkgs/plugins/neogit/generated.nix
@@ -3,14 +3,14 @@
 {
   neogit = {
     pname = "neogit";
-    version = "38dd297a905ec6869f4c20ea9184a3e514316e3b";
+    version = "ce0c369ccdba3f644a3b28f4c053421f435352c9";
     src = fetchFromGitHub {
       owner = "NeogitOrg";
       repo = "neogit";
-      rev = "38dd297a905ec6869f4c20ea9184a3e514316e3b";
+      rev = "ce0c369ccdba3f644a3b28f4c053421f435352c9";
       fetchSubmodules = false;
-      sha256 = "sha256-TPsOHWvBj0D/vJQENcmYf2HvA3tZ+C1Xo7klqZsh90k=";
+      sha256 = "sha256-wNVcRGOu58Yj66A7isRy9RRaVVP0mkG0M+h8+J43en8=";
     };
-    date = "2024-01-16";
+    date = "2024-01-22";
   };
 }
