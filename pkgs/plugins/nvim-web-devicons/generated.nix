e7a74419148c2f0e2c8d64a2003959f85f6dbe61
polarmutex <polarmutex@users.noreply.github.com>
2024-01-25 21:13:59 +0000

    chore(plugin-update):  nvim-web-devicons: 140edfcf25093e8b321d13e154cbce89ee868ca0 → b427ac5f9dff494f839e81441fb3f04a58cbcfbc

--- pkgs/plugins/nvim-web-devicons/generated.nix
+++ pkgs/plugins/nvim-web-devicons/generated.nix
@@ -3,14 +3,14 @@
 {
   nvim-web-devicons = {
     pname = "nvim-web-devicons";
-    version = "140edfcf25093e8b321d13e154cbce89ee868ca0";
+    version = "b427ac5f9dff494f839e81441fb3f04a58cbcfbc";
     src = fetchFromGitHub {
       owner = "nvim-tree";
       repo = "nvim-web-devicons";
-      rev = "140edfcf25093e8b321d13e154cbce89ee868ca0";
+      rev = "b427ac5f9dff494f839e81441fb3f04a58cbcfbc";
       fetchSubmodules = false;
-      sha256 = "sha256-xuHIdvezGH04X+B+x4MjaUFOAZkR2Qc19ErZaeUJ7W0=";
+      sha256 = "sha256-w038PU9i1onEBo3x4bo1kDz9Fo46Whd8ZJhyIqxz3I8=";
     };
-    date = "2024-01-15";
+    date = "2024-01-24";
   };
 }
