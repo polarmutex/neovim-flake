238fbf732f71f2c6ddaba71d980b9661bd37ef91
polarmutex <polarmutex@users.noreply.github.com>
2024-03-08 04:05:56 +0000

    chore(plugin-update):  lualine-nvim: 7d131a8d3ba5016229e8a1d08bf8782acea98852 → 8b56462bfb746760465264de41b4907310f113ec

--- pkgs/plugins/lualine-nvim/generated.nix
+++ pkgs/plugins/lualine-nvim/generated.nix
@@ -3,14 +3,14 @@
 {
   lualine-nvim = {
     pname = "lualine-nvim";
-    version = "7d131a8d3ba5016229e8a1d08bf8782acea98852";
+    version = "8b56462bfb746760465264de41b4907310f113ec";
     src = fetchFromGitHub {
       owner = "nvim-lualine";
       repo = "lualine.nvim";
-      rev = "7d131a8d3ba5016229e8a1d08bf8782acea98852";
+      rev = "8b56462bfb746760465264de41b4907310f113ec";
       fetchSubmodules = false;
-      sha256 = "sha256-LMMcRY4qnGywdK6Bl4YeAEKLhnRuOZ2txn4oYoso2gI=";
+      sha256 = "sha256-G4npmdS7vue68h5QN8vWx3Oh5FHdvmzFHGxqMIRC+Mk=";
     };
-    date = "2024-01-25";
+    date = "2024-03-04";
   };
 }
