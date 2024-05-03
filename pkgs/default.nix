82f86db95c476c40aa8c893599425696d36d50b6
polarmutex <polarmutex@users.noreply.github.com>
2024-05-03 16:50:08 -0400

    ci: fix errors in ci

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -49,7 +49,7 @@ in {
 
       inherit (pkgs) fd;
       inherit (pkgs) jq;
-      inherit (pkgs) nvfetcher;
+      inherit (pkgs) npins;
 
       # from https://github.com/nix-community/neovim-nightly-overlay
       neovim-git = inputs'.neovim-flake.packages.neovim;
@@ -133,7 +133,37 @@ in {
             usage
           else
             # https://stackoverflow.com/questions/51217020/jq-convert-array-to-object-indexed-by-filename
-            jq -cn '[ inputs | .pins | keys_unsorted | { name: .[], "sources-file": input_filename } ]' "$@"
+            # jq -cn '[ inputs | .pins | keys_unsorted | { name: .[], "sources-file": input_filename } ]' "$@"
+            jq -cn '[ inputs | .pins | map(.) | .[] | { name: .repository.repo, version: (if .version != null then .version else .revision[0:8] end), "sources-file": input_filename } ]' "$@"
+          fi
+        '';
+      };
+
+      npins-version-matrix = pkgs.writeShellApplication {
+        name = "npins-version-matrix";
+        runtimeInputs = with pkgs; [
+          coreutils
+          jq
+        ];
+
+        text = ''
+          #!/bin/bash
+
+          usage() {
+            printf "%s\n\n" "usage: $(basename "$0") <source-file> <package-name>"
+            printf "%s\n\n" "Prints a JSON array of objects that each correspond to an \`npins\` source."
+            printf "%s\n\n" "Each value in <source-files...> should be a path to an \`npins\` \`sources.json\` file."
+            printf "%s\n" "Each object in the output contains the following keys:"
+            printf "  %s\n" "\"name\": the name of the pin as per its entry in its \`sources.json\`"
+            printf "  %s\n" "\"sources-file\": the path to the \`sources.json\` file that contains the pin"
+            exit 1
+          }
+
+          if [ $# -eq 0 ]; then
+            usage
+          else
+            # https://stackoverflow.com/questions/51217020/jq-convert-array-to-object-indexed-by-filename
+            jq -cn "inputs | .pins | .[] |select(.repository.repo == \"$2\") |  if .version != null then .version else .revision[0:8] end " "$1"
           fi
         '';
       };
