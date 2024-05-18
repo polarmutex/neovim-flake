ad10a0dcf28df3d415bb5b5e338fb8af5647c091
polarmutex <polarmutex@users.noreply.github.com>
2024-05-17 21:12:37 -0400

    chore(plugin/update): nvim-cmp: 24122371 -> 5260e5e8

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -204,7 +204,7 @@ in {
 
         text = ''
           OLD_VERSION=$(jq -rcn "inputs | .pins | .[] |select(.repository.repo == \"$1\") |  if .version != null then .version else .revision[0:8] end " "pkgs/npins/sources.json")
-          ${pkgs.npins}/bin/npins -d pkgs/npins update $1
+          ${pkgs.npins}/bin/npins -d pkgs/npins update "$1"
           NEW_VERSION=$(jq -rcn "inputs | .pins | .[] |select(.repository.repo == \"$1\") |  if .version != null then .version else .revision[0:8] end " "pkgs/npins/sources.json")
           git commit -am "chore(plugin/update): $1: $OLD_VERSION -> $NEW_VERSION"
         '';
