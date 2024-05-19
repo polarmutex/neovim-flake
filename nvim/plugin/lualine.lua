faae7fdd3aa0b6952490b6f8f8bcc10ea814af85
polarmutex <polarmutex@users.noreply.github.com>
2024-05-19 12:59:36 -0400

    fix: deprecated lsp function

--- nvim/plugin/lualine.lua
+++ nvim/plugin/lualine.lua
@@ -19,7 +19,7 @@ local lsp_active_section = {
     function()
         local msg = "No Active Lsp"
         local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-        local clients = vim.lsp.get_active_clients()
+        local clients = vim.lsp.get_clients()
         if next(clients) == nil then
             return msg
         end
