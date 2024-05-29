fdbdc4eb2e1a81db157fe0d0794f5e7abf65d8fb
polarmutex <polarmutex@users.noreply.github.com>
2024-05-29 12:49:21 -0400

    fix: more kickstart.lua

--- nvim/plugin/dap.lua
+++ nvim/plugin/dap.lua
@@ -25,16 +25,19 @@ local map = function(lhs, rhs, desc)
     vim.keymap.set("n", lhs, rhs, { silent = true, desc = desc })
 end
 
-map("<F5>", require("dap").continue, "continue")
-map("<F9>", require("dap").step_back, "step back")
-map("<F10>", require("dap").step_over, "step over")
-map("<F11>", require("dap").step_into, "step into")
-map("<F12>", require("dap").step_out, "step out")
+map("<F5>", require("dap").continue, "Debug: Continue")
+map("<F9>", require("dap").step_back, "Debug: Step Back")
+map("<F10>", require("dap").step_over, "Debug: Step Over")
+map("<F11>", require("dap").step_into, "Debug: Step Into")
+map("<F12>", require("dap").step_out, "Debug: Step Out")
 
-map("<leader>db", require("polarmutex.utils.dap").toggle_breakpoint)
-map("<leader>dB", function()
-    require("dap").set_breakpoint(vim.fn.input("[DAP] Condition > "))
-end, "conditional breakpoint")
+vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
+vim.keymap.set("n", "<leader>B", function()
+    dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
+end, { desc = "Debug: Set Breakpoint" })
+
+-- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
+vim.keymap.set("n", "<F7>", dapui.toggle, { desc = "Debug: See last session result." })
 
 map("<leader>dr", require("dap").repl.open, "repl")
 
@@ -44,7 +47,23 @@ map("<leader>dE", function()
 end, "ui eval input")
 
 dapui.setup({
-    --icons = { expanded = "▾", collapsed = "▸" },
+    -- Set icons to characters that are more likely to work in every terminal.
+    --    Feel free to remove or use ones that you like more! :)
+    --    Don't feel like these are good choices.
+    icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
+    controls = {
+        icons = {
+            pause = "⏸",
+            play = "▶",
+            step_into = "⏎",
+            step_over = "⏭",
+            step_out = "⏮",
+            step_back = "b",
+            run_last = "▶▶",
+            terminate = "⏹",
+            disconnect = "⏏",
+        },
+    },
     --mappings = {
     --    expand = { "<CR>", "<2-LeftMouse>" },
     --    open = "o",
