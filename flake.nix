4d9dfee79aec93210e7d84a5a45470978b5f3c43
Brian Ryall <brianryall@fastmail.com>
2024-05-31 21:40:03 -0400

    fix: add x86_64 darwin support

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
   } @ inputs:
     flake-parts.lib.mkFlake {inherit inputs;}
     {
-      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
+      systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
 
       imports = [
         ./checks
