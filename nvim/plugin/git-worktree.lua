315cc7aec830ab9c89373fbae928d7d1ed8e25e6
polarmutex <polarmutex@users.noreply.github.com>
2024-07-17 23:36:22 -0400

    fix: git-worktree config for latest changes

--- nvim/plugin/git-worktree.lua
+++ nvim/plugin/git-worktree.lua
@@ -1,9 +1,7 @@
-local gwt = require("git-worktree")
-gwt.setup({})
+-- local gwt = require("git-worktree")
+--gwt.setup({})
 
 require("telescope").load_extension("git_worktree")
 
 local Hooks = require("git-worktree.hooks")
-Hooks.hooks:add_listener({
-    SWITCH = Hooks.builtins.update_current_buffer_on_switch,
-})
+Hooks.register(Hooks.type.SWITCH, Hooks.builtins.update_current_buffer_on_switch)
