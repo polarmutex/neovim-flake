938eaa1ff8345e6ab0915208e30c935f8f26d2e0
polarmutex <polarmutex@users.noreply.github.com>
2024-07-17 23:35:02 -0400

    feat: add hardmode enable

--- nvim/plugin/hardmode.lua
+++ nvim/plugin/hardmode.lua
@@ -2,5 +2,9 @@ require("hardtime").setup({
     enabled = false,
     disabled_filetypes = {},
 })
-require("precognition").setup({})
-require("precognition").hide()
+require("precognition").setup({ startVisible = false })
+
+vim.keymap.set("n", "<leader>hh", function()
+    require("precognition").toggle()
+    require("hardtime").toggle()
+end, { desc = "[S]earch [H]elp" })
