d514b3e983f123888f0334b5a2d248e8f5121be9
polarmutex <polarmutex@users.noreply.github.com>
2024-10-21 11:11:30 -0400

    fix: add reuse client to python

--- pkgs/polar-init-config/ftplugin/python.lua
+++ pkgs/polar-init-config/ftplugin/python.lua
@@ -44,9 +44,9 @@ local config = {
 }
 
 vim.lsp.start(config, {
-    -- reuse_client = function(client, conf)
-    --     return client.name == conf.name and client.config.root_dir == conf.root_dir
-    -- end,
+    reuse_client = function(client, conf)
+        return client.name == conf.name and client.config.root_dir == conf.root_dir
+    end,
 })
 
 require("dap-python").setup()
